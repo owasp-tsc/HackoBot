1b0022f53b390125892549f562cf55fa56a7171d
Rishabh <rjain_be19@thapar.edu>
2021-03-25 20:29:17 +0530

    u

--- bot/config.js
+++ bot/config.js
@@ -28,7 +28,7 @@ module.exports = {
     kickThreshold: 5, // Amount of messages sent in a row that will cause a kick.
     banThreshold: 7, // Amount of messages sent in a row that will cause a ban.
     maxInterval: 5000, // Amount of time (in milliseconds) in which messages are considered spam.
-    warnMessage: "{@user}, Please stop spamming.", // Message that will be sent in chat upon warning a user.
+    warnMessage: "{@user}, Please stop spamming. ", // Message that will be sent in chat upon warning a user.
     kickMessage: "**{user_tag}** has been kicked for spamming.", // Message that will be sent in chat upon kicking a user.
     banMessage: "**{user_tag}** has been banned for spamming.", // Message that will be sent in chat upon banning a user.
     maxDuplicatesWarning: 4, // Amount of duplicate messages that trigger a warning.
